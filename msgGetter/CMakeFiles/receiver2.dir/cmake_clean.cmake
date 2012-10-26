cbffeb6b777221001ff6171961675aa786bb2a52
Sergey Kouzmin <kouzmin.s.n@smilart.com>
2012-10-26 15:19:06 +0400

    add msgGetter

---
+++ msgGetter/CMakeFiles/receiver2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/receiver2.dir/receiver2.cpp.o"
+  "receiver2.pdb"
+  "receiver2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/receiver2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
